80566200a0624368fa376533f266a0ba266f080b
Nguyen Van Hoa <126589828+nguyenhoa-ize@users.noreply.github.com>
2024-10-15 08:45:35 +0700

    create project

---
+++ AuctionKoi/src/test/java/uth/edu/auctionkoi/AuctionKoiApplicationTests.java
@@ -0,0 +1,13 @@
+package uth.edu.auctionkoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionKoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
