8a17f4953931f89bb475df65e95f56e907121aa6
Khanh5604 <biphingua@gmail.com>
2024-10-23 21:11:54 +0700

    thiết kế phần login

--- AuctionKoi/src/main/resources/templates/javascript.js
+++ AuctionKoi/src/main/resources/templates/javascript.js
@@ -1,31 +1,43 @@
+// sự kiện click vào các thẻ trên thanh menu
 $(document).ready(function() {
     $('.about').on('click', function(event) {
         event.preventDefault();
-        $('#content, #slider, #footer, .Hocakoi, #blogs-section, #contact-section, #auction-section, #product-section').hide();
+        $('#content, #slider, #footer, .Hocakoi, #blogs-section, #auction-section, #product-section').hide();
         $('#about-section').show();
     });
 
     $('.blogs').on('click', function(event) {
         event.preventDefault();
-        $('#content, #slider, #footer, .Hocakoi, #about-section, #contact-section, #auction-section, #product-section').hide();
+        $('#content, #slider, #footer, .Hocakoi, #about-section, #auction-section, #product-section').hide();
         $('#blogs-section').show();
-    });
-
-    $('.contact').on('click', function(event) {
-        event.preventDefault();
-        $('#content, #slider, #footer, .Hocakoi, #about-section, #blogs-section, #auction-section, #product-section').hide();
-        $('#contact-section').show();
     });
 
     $('.auction').on('click', function(event) {
         event.preventDefault();
-        $('#content, #slider, #footer, .Hocakoi, #about-section, #blogs-section, #contact-section, #product-section').hide();
+        $('#content, #slider, #footer, .Hocakoi, #about-section, #blogs-section, #product-section').hide();
         $('#auction-section').show();
     });
 
     $('.product').on('click', function(event) {
         event.preventDefault();
-        $('#content, #slider, #footer, .Hocakoi, #about-section, #blogs-section, #contact-section, #auction-section').hide();
+        $('#content, #slider, #footer, .Hocakoi, #about-section, #blogs-section,#auction-section').hide();
         $('#product-section').show();
     });
+    $('.active, .tieude').click(function (){
+        window.location.href = 'index.html';
+    })
 });
+
+// sự kiện của login
+$(document).ready(function () {
+    $('.login').click(function (event) {
+        event.preventDefault();
+        $('#mau_nen').show();
+        $('#tk_login').show();
+    });
+    $('#mau_nen').click(function () {
+        $(this).hide();
+        $('#tk_login').hide();
+    });
+});
+
